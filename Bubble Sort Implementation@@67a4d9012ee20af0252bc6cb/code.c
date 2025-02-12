5c99cb98e15e74db483306ce7f381152a439389a
Raghav Jindal <raghav1jindal@gmail.com>
2025-02-12 13:54:01 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,41 +1,42 @@
-// #include<stdio.h>
-// #define rep(i,a,b) for(int i = a;i<b;i++)
-// void bubbleSort(int arr[], int n);
-// void printArray(int arr[], int n);
-// int main()
-// {
-//     int n;
-//     scanf("%d", &n);
-//     int arr[n];
-//     rep(i,0,n)
-//     {
-//         scanf("%d", &arr[i]);
-//     }
-//     bubbleSort(arr, n);    
-//     printArray(arr, n);
+#include<stdio.h>
+#include<stdlib.h>
+#define rep(i,a,b) for(int i = a;i<b;i++)
+void bubbleSort(int arr[], int n);
+void printArray(int arr[], int n);
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int *arr = (int*)malloc(n*sizeof(int));
+    rep(i,0,n)
+    {
+        scanf("%d", &arr[i]);
+    }
+    bubbleSort(arr, n);    
+    printArray(arr, n);
 
-//     return 0;
-// }
-// void bubbleSort(int arr[], int n)
-// {
-//     rep(i,1,n)
-//     {
-//         rep(k,0,n-i)
-//         {
-//             if(arr[k] > arr[k+1])
-//             {
-//                 int temp = arr[k];
-//                 arr[k] = arr[k+1];
-//                 arr[k+1] =  temp;
-//             }
-//         }
-//     }
-// }
-// void printArray(int arr[], int n)
-// {
-//     rep(i,0,n)
-//     {
-//         printf("%d ",arr[i]);
-//     }
-// }
+    return 0;
+}
+void bubbleSort(int arr[], int n)
+{
+    rep(i,1,n)
+    {
+        rep(k,0,n-i)
+        {
+            if(arr[k] > arr[k+1])
+            {
+                int temp = arr[k];
+                arr[k] = arr[k+1];
+                arr[k+1] =  temp;
+            }
+        }
+    }
+}
+void printArray(int arr[], int n)
+{
+    rep(i,0,n)
+    {
+        printf("%d ",arr[i]);
+    }
+}
 
